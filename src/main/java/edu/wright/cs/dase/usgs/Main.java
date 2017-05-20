81105566c621bfb05ce01bff8549451a94399962
Michelle Cheatham <michelle.cheatham@gmail.com>
2017-05-20 10:15:17 -0400

    Hello world

---
+++ src/main/java/edu/wright/cs/dase/usgs/Main.java
@@ -0,0 +1,10 @@
+package edu.wright.cs.dase.usgs;
+
+import static spark.Spark.*;
+
+public class Main {
+	public static void main(String[] args) {
+		port(8080);
+		get("/hello", (req, res) -> "Hello World");
+	}
+}
