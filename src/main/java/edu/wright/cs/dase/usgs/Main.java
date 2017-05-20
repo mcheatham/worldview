4d96acdc2f987b3425e860927e2a5f20ac42ea2d
Michelle Cheatham <michelle.cheatham@gmail.com>
2017-05-20 10:36:42 -0400

    Serve static files, sample handler

--- src/main/java/edu/wright/cs/dase/usgs/Main.java
+++ src/main/java/edu/wright/cs/dase/usgs/Main.java
@@ -3,8 +3,19 @@ package edu.wright.cs.dase.usgs;
 import static spark.Spark.*;
 
 public class Main {
+	
 	public static void main(String[] args) {
 		port(8080);
+		staticFiles.location("/public");
 		get("/hello", (req, res) -> "Hello World");
+		
+		get("/example", (req, res) -> 
+			example(Integer.parseInt(req.queryParams("first")), 
+					Integer.parseInt(req.queryParams("second"))));
+	}
+	
+	
+	public static int example(int a, int b) {
+		return a + b;
 	}
 }
